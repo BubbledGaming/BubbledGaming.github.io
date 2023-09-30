a6779f83a6ba31d95a8d897cf6dbb5ada5c516bd
BubbledGaming <30528934+BubbledGaming@users.noreply.github.com>
2023-09-30 17:33:17 -0400

    added a thing I guess
    :/

---
+++ scripts/WebpageChange.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var changelogButton = document.getElementById("changelogButton");
+    changelogButton.addEventListener("click", function() {
+        window.location.href = "changelog.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var tracklistButton = document.getElementById("tracklistButton");
+    tracklistButton.addEventListener("click", function() {
+        window.location.href = "tracklist.html";
+    });
+});
